d18470a380376fc71109e24d4d579a1f2c2e302a
Nodirjonov <sobirovakomila85@gmail.com>
2024-03-27 17:48:11 +0500

    5oy2dars

---
+++ script.js
@@ -0,0 +1,27 @@
+let a = prompt('name kiriting')
+let b = prompt('id kiriting')
+let c = prompt('parol kiriting')
+
+
+const user ={
+    name:'name kiritmadingiz',
+    id:"id kiritmadingiz",
+    parol:'parol kiritmadingiz',
+
+
+
+};
+user.name=a
+// console.log(user['name']);
+user.id=b
+// console.log(user['id']);
+user.parol=c
+// console.log(user['parol']);
+console.log(user);
+
+
+
+
+
+
+
